35539d6680783531bb3d7e58a20ad34423706cd4
ongraphpythondev <pythondev@ongraph.com>
2022-02-21 08:57:37 +0530

    second commit

--- EcomerceApi/DjangoAPIapp/models.py
+++ EcomerceApi/DjangoAPIapp/models.py
@@ -29,20 +29,22 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-    class Product(models.Model):
-        product_tag = models.CharField(max_length=10)
-        name = models.CharField(max_length=100)
-        category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
-        price = models.IntegerField()
-        stock = models.IntegerField()
-        imageUrl = models.URLField()
-        status = models.BooleanField(default=True)
-        date_created = models.DateField(auto_now_add=True)
+class Product(models.Model):
 
-        class Meta:
+    product_tag = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,related_name='products',on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    imageUrl = models.URLField()
+    status = models.BooleanField(default=True)
+    date_created = models.DateField(auto_now_add=True)
+
+
+    class Meta:
             ordering =['-date_created']
 
-        def __str__(self):
+    def __str__(self):
             return'{} {}'.format(self.product_tag,self.name)
             
                         
